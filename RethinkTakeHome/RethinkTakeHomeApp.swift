18bf752cd91bdac503584c46648fa4b9e5b1de60
Taylor Simpson <taylors.onelight@gmail.com>
2023-05-10 18:39:47 -0700

    Initial Commit

---
+++ RethinkTakeHome/RethinkTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RethinkTakeHomeApp.swift
+//  RethinkTakeHome
+//
+//  Created by Taylor Simpson on 5/10/23.
+//
+
+import SwiftUI
+
+@main
+struct RethinkTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
